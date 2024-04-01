172453e935cb90589e1a561cc14ad6b7c19f8f91
adubasava <anzhalikad@gmail.com>
2024-04-01 18:51:34 +0300

    Add caesar cipher function

---
+++ caesar-cipher.js
@@ -0,0 +1,20 @@
+﻿function caesarCipher(string, factor) {
+    let oldCode;
+    let newCode;
+    let newString = "";
+    for (let i = 0; i < string.length; i++) {
+        oldCode = string.charCodeAt(i);
+        newCode = oldCode + factor % 26;
+        if (newCode > 122 || (newCode > 90 && oldCode < 91)) {
+            newCode = newCode - 26;
+        }        
+        if (oldCode < 65 || (oldCode < 97 && oldCode > 90) || oldCode > 122) {
+            newString += String.fromCharCode(oldCode);
+        } else {
+            newString += String.fromCharCode(newCode);
+        }        
+    }
+    return newString 
+}
+
+export default caesarCipher
---
+++ caesar-cipher.test.js
@@ -0,0 +1,10 @@
+﻿import caesarCipher from './caesar-cipher';
+
+test('encrypt', () => {
+    expect(caesarCipher('abc', 1)).toMatch(/bcd/);
+    expect(caesarCipher('ABC', 1)).toMatch(/BCD/);
+    expect(caesarCipher('a', 144)).toMatch(/o/);
+    expect(caesarCipher('z', 1)).toMatch(/a/);
+    expect(caesarCipher('Z', 1)).toMatch(/A/);
+    expect(caesarCipher('Z, z', 1)).toMatch(/A, a/);
+  });
